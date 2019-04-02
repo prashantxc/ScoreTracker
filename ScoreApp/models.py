dd529810ff25b40cf0fabf6339752e5832bcfdf1
Prashant Singh <prashantxc8@gmail.com>
2019-04-02 20:47:42 +0530

    New Model: Match

--- ScoreApp/models.py
+++ ScoreApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Match(models.Model):
+    PlayerOne = models.CharField(max_length=100)
+    PlayerTwo = models.CharField(max_length=100)
+    Score = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = (('PlayerOne', 'PlayerTwo'),)
