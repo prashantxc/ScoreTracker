46cd26b5e00be9e3e503b45b00c0b4c806a3cf77
Prashant Singh <prashantxc8@gmail.com>
2019-04-03 00:57:38 +0530

    Record Page: Navbar

--- ScoreApp/views.py
+++ ScoreApp/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 from .models import *
 
 # Create your views here.
+def Records(request):
+    return render(request, 'records.html')
--- ScoreTracker/urls.py
+++ ScoreTracker/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ScoreApp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Records)
 ]
